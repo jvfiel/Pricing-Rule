ead9047bb98bf102d6446db31818bc78db003003
jvfiel <jvfiel@bai.ph>
2019-09-08 01:28:02 +0800

    apply pricing rule for item group

--- pricing_rule/pricing_rule/__init__.py
+++ pricing_rule/pricing_rule/__init__.py
@@ -2,13 +2,23 @@ import frappe
 
 @frappe.whitelist()
 def get_pricing(item,uom):
+
+    item_group = frappe.db.sql("""SELECT item_group FROM `tabItem` WHERE item_code=%s""",(item))
+
+
     pricing_rule = frappe.db.sql("""SELECT * FROM `tabPricing Rule`
-                                WHERE item_code=%s AND uom=%s""",(item,uom),as_dict=True)
+                                WHERE (item_code=%s OR item_group=%s)
+                                AND uom=%s""",(item,item_group[0][0],uom),
+                                 as_dict=True)
 
-    frappe.db.sql("""UPDATE `tabPricing Rule` SET priority = '' WHERE item_code=%s AND uom != %s""",(item,uom))
+    if pricing_rule != ():
 
-    frappe.db.sql("""UPDATE `tabPricing Rule` SET priority = '1' WHERE item_code=%s AND uom = %s""",(item,uom))
+        frappe.db.sql("""UPDATE `tabPricing Rule` SET priority = ''
+                WHERE (item_code=%s OR item_group=%s)  AND uom != %s""",(item,item_group[0][0],uom))
 
-    frappe.db.commit()
+        frappe.db.sql("""UPDATE `tabPricing Rule` SET priority = '1'
+              WHERE (item_code=%s OR item_group=%s)  AND uom = %s""",(item,item_group[0][0],uom))
+
+        frappe.db.commit()
 
     return pricing_rule
