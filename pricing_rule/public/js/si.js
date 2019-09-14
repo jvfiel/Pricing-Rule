300f503ad4ce008dd2a5f95e1c0ba3ac031023af
jvfiel <jvfiel@bai.ph>
2019-09-14 09:59:11 +0800

    conversion factor

--- pricing_rule/public/js/si.js
+++ pricing_rule/public/js/si.js
@@ -124,36 +124,36 @@ frappe.ui.form.on("Sales Invoice Item", {
     }
     ,
     uom: function (doc, cdt, cdn) {
-          let me = this;
-        let row = frappe.get_doc(cdt, cdn);
-        console.log("pricing rule**");
-        console.log(row);
-
-            if (row.item_code && row.uom) {
-                frappe.call({
-                    method: "pricing_rule.pricing_rule.get_pricing",
-                    args: {
-                        item: row.item_code,
-                        uom: row.uom,
-                        pricelist:cur_frm.doc.selling_price_list
-                    },
-                    callback: function (r) {
-                        console.log(r);
-                        if(!isEmpty(r.message)) {
-                            // row.margin_type = "Percentage";
-                            // row.discount_percentage = r.message[0].discount_percentage;
-                            console.log(r.message[0].name);
-                            row.pricing_rule = r.message[0].name;
-                            console.log(r.message[0].discount_percentage);
-                            row.discount_percentage = r.message[0].discount_percentage;
-                        }
-                        else {
-                            row.pricing_rule = "";
-                            frappe.msgprint("no Princing Rule found.");
-                        }
-                        refresh_field("items");
-                    }
-                });
-            }
+        //   let me = this;
+        // let row = frappe.get_doc(cdt, cdn);
+        // console.log("pricing rule**");
+        // console.log(row);
+        //
+        //     if (row.item_code && row.uom) {
+        //         frappe.call({
+        //             method: "pricing_rule.pricing_rule.get_pricing",
+        //             args: {
+        //                 item: row.item_code,
+        //                 uom: row.uom,
+        //                 pricelist:cur_frm.doc.selling_price_list
+        //             },
+        //             callback: function (r) {
+        //                 console.log(r);
+        //                 if(!isEmpty(r.message)) {
+        //                     // row.margin_type = "Percentage";
+        //                     // row.discount_percentage = r.message[0].discount_percentage;
+        //                     console.log(r.message[0].name);
+        //                     row.pricing_rule = r.message[0].name;
+        //                     console.log(r.message[0].discount_percentage);
+        //                     row.discount_percentage = r.message[0].discount_percentage;
+        //                 }
+        //                 else {
+        //                     row.pricing_rule = "";
+        //                     frappe.msgprint("no Princing Rule found.");
+        //                 }
+        //                 refresh_field("items");
+        //             }
+        //         });
+        //     }
     }
 });
